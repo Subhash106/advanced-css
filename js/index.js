a68df6e9e74e6d76ebaa9021ac953631e4de9619
Subash Chandra <subash_chandra@crgl-thirdparty.com>
2023-02-06 05:38:52 +0530

    Added responsive design

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
-const myStr = "hello there";
-const myObj = {};
-for (let i = 0; i < myStr.length; i++) {
-  if (myObj[myStr[i]]) {
-    myObj[myStr[i]]++;
-  } else {
-    myObj[myStr[i]] = 1;
-  }
-}
+// test1();
 
-console.log(myObj);
+// function test1() {
+//   console.log("test1 called");
+// }
 
-for (let data in myObj) {
-  console.log(`${data}=>${myObj[data]}`);
-}
+// console.log(test2);
+
+// var test2 = function () {
+//   console.log("test2 called");
+// };
+
+// const myStr = "hello there";
+// const myObj = {};
+// for (let i = 0; i < myStr.length; i++) {
+//   if (myObj[myStr[i]]) {
+//     myObj[myStr[i]]++;
+//   } else {
+//     myObj[myStr[i]] = 1;
+//   }
+// }
+
+// console.log(myObj);
+
+// for (let data in myObj) {
+//   console.log(`${data}=>${myObj[data]}`);
+// }
